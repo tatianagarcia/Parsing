5b08cb8a808b4e43b80609c60688e6d0dc721cde
tatianagarcia <tatianagarcia@dhcp-10-202-65-24.tamulink.tamu.edu>
2016-10-20 16:14:34 -0500

    Complete Change
    Restarted, with posted work as reference

--- q1.rb
+++ q1.rb
@@ -1,2 +1,30 @@
-logs = File.open("User/tatianagarcia/Desktop/Ruby/http_access_log")
-puts ="The total amount of request is: #{logs.readlines.size}" 
+require 'date'
+require 'open-uri'
+
+REMOTE_FILE = 'http://s3.amazonaws.com/tcmg412-fall2016/http_access_log'
+MY_FILE = 'http_access_log'
+LOG_FILES = 'logs/'
+
+i = 0
+by_month = {}
+files = {}
+sums = {}
+errors = []
+
+print "downloading"
+download open(REMOTE_FILE)
+IO.copy_stream(download, MY_FILE)
+puts "downloaded" 
+
+File.foreach(MY_FILE) do |line|
+  
+  regex = /.* \[(.*) .*\] "([A-Z]{3,4}) (.*) .*" (\d{3}) .*/.match(line)
+  
+  if !regex then 
+          errors.push(line)
+          next
+  end
+  
+  
+
+
